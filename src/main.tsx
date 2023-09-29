492ecd9f4b14f9c704bdc64a1fd96e234904347a
trinitytrin <choudhury.trina@gmail.com>
2023-09-29 10:35:45 +0600

    routing in taklamethod

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../style.css';
-import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import router from './routing/router';
 
 
