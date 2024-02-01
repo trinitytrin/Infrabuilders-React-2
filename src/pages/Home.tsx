5fca1e6cce72c8afc272380d485549e3e465fe2f
trinitytrin <choudhury.trina@gmail.com>
2024-02-01 20:27:11 +0600

    fixed page css break issue on reload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,11 @@ import ClientTestimonial from "../components/home/testimonial/ClientTestimonial"
 import Counter from "../components/home/Counter";
 import ShowcaseHome from "../components/home/ShowcaseHome";
 import WorkProgressBar from "../components/home/WorkProgressBar";
-import { Helmet, HelmetProvider } from "react-helmet-async";
+
 
 const Home = () => {
     return (
-        <HelmetProvider>
+        <>
             <BannerHome />
             <div className="content">
                 <ShowcaseHome />
@@ -20,20 +20,7 @@ const Home = () => {
             </div>
 
 
-            <Helmet>
-                <script src='/js/modernizr-2.8.3.min.js' ></script>
-                <script src='/js/plugins.js' type='module' async></script>
-                <script src='/js/main.js'  ></script>
-                <script src="js/ajax-mail.js" type='module'></script>
-
-                <link rel="stylesheet" href="/css/elements.css" />
-                <link rel="stylesheet" href="/style.css" />
-                <link rel="stylesheet" href="/css/responsive.css" />
-                <link rel="stylesheet" href="/css/custom.css" />
-                <link rel="stylesheet" href="/src/App.css" />
-            </Helmet >
-
-        </HelmetProvider >
+        </>
     )
 }
 
